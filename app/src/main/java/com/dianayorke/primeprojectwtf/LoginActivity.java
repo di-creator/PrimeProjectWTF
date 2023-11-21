6867317ba7cd1d823f5dea4b3f5b7f0db35ea47e
dicreator <yorkedianaba@gmail.com>
2023-11-21 17:41:15 +0100

    New commit change as at 21st Nov

---
+++ app/src/main/java/com/dianayorke/primeprojectwtf/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.dianayorke.primeprojectwtf;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
--- app/src/main/java/com/dianayorke/primeprojectwtf/MainActivity.kt
+++ app/src/main/java/com/dianayorke/primeprojectwtf/MainActivity.kt
@@ -2,10 +2,19 @@ package com.dianayorke.primeprojectwtf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val joinButton: Button = this.findViewById(R.id.joinButton)
+
+        joinButton.setOnClickListener {
+            val intent = Intent()
+        }
+
     }
 }
